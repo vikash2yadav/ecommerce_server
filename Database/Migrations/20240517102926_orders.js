a8d9f6f67e9764a541ab67f61db177340c5a5eb7
VikashY-ITL <vikas.y@intutechlab.com>
2024-05-17 16:08:43 +0530

    initilized brach

---
+++ Database/Migrations/20240517102926_orders.js
@@ -0,0 +1,28 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.BIGINT(20).UNSIGNED
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('orders');
+  }
+};
